1fbfc199354a4fbc0aa361ad30d98b104da72c42
James M. Jensen II <jjensen@jsu.edu>
2013-05-10 21:27:16 -0500

    Second (failed) attempt.

--- curry-lisp.py
+++ curry-lisp.py
@@ -1,64 +1,126 @@
 ### Curry Lisp
+### Take 2
 
 import string
+from aastruct import struct
 
-class Unbound:
-    def __init__ (self, symbol):
-        self.value = "Unbound: " + symbol
-        self.symbol = symbol
-    def __call__ (self, context, arg):
-        if self.symbol in context:
-            return context[self.symbol]
+class context:
+    def __init__ (self, members={}, parent=None):
+        self.__parent = parent
+        self.__members = members.copy()
+
+    def __getitem__ (self, key):
+        if key in self.__members:
+            return self.__members[key]
         else:
-            raise Exception(self.symbol)
+            if self.__parent is None:
+                raise KeyError(key)
+            else:
+                return self.__parent[key]
 
-def with_value(fn, value):
-    fn.value = value
-    return fn
+    def __setitem__ (self, key, value):
+        self.__members[key] = value
 
-global_context = {
-    None: None,
+    def __iter__ (self):
+        return self.__members.iterkeys()
 
-    # \ is lambda
-    '\\': with_value(lambda c1, argName: lambda c2, value: create_function(argName, value, context=dict(c1, **c2)),
-                     ['<special form "\\">']),
-    ':=': with_value(lambda c1, key: lambda c2, value: global_context.update({key: value if callable(value) else evaluate(value)}),
-                     ['<special form ":=">'])
-}
 
-def trampoline (f):
-    while callable(f):
-        f = f()
-    return f
+class unbound:
+    def __init__ (self, symbol):
+        self.__doc__ = "Unbound: " + str(symbol)
+        self.__symbol = symbol
+    def __call__ (self, context, arg):
+        if self.__symbol in context:
+            return context[self.__symbol]
+        else:
+            raise KeyError(self.__symbol)
 
-def evaluate(value, context=global_context):
-    if isinstance(value, str):
-        if value in context:
+    def __str__(self):
+        return repr(self)
+
+    def __repr__(self):
+        return self.__doc__
+
+
+# Basic stuff
+nil = struct({'__doc__': 'nil'})
+nil.first = lambda: nil
+nil.rest = lambda: nil
+
+def cons (f, r):
+    return struct({'first': lambda: f, 'rest': lambda: r})
+
+
+def symbol (sym):
+    return with_doc(cons(sym, nil), "symbol: " + str(sym))
+
+
+# Helpers
+def doc (obj):
+    return obj.__doc__
+
+
+def with_doc (obj, doc):
+    class c:
+        def __call__ (self, c, expr):
+            obj(c, expr)
+        def __repr__(self):
+            return doc
+        def __str__(self):
+            return repr(self)
+
+    return c()
+
+
+# Primitive functions
+def create_function (context, argName, body):
+    def anon (c, expr):
+        return evaluate(context({argName: evaluate(c, expr)}, c), body)
+    return anon
+
+
+def evaluate (context, expr):
+    if isinstance(expr, str):
+        if expr in context:
             return context[value]
         else:
-            return Unbound(value)
-    elif isinstance(value, list):
+            return unbound(value)
+    elif isinstance(expr, list):
         # Start with identity function
         fn = lambda c, x: x
 
-        for expr in value:
-            fn = fn(context, expr)
+        for exp in expr:
+            fn = fn(context, exp)
             if not callable(fn):
                 if (isinstance(fn, list)):
                     fn = evaluate(fn, context=context)
                 elif fn in context:
                     fn = context[fn]
                 else:
-                    # Hack
-                    old_fn = fn
-                    fn = lambda c, x: old_fn
-                    fn.value = old_fn
+                    fn = unbound(fn)
         return fn
 
-def create_function(argName, value, context):
-    result = lambda c, arg: evaluate(value, context=dict(dict(context, **c), **{argName: evaluate(arg, context=dict(context, **c))}))
-    result.value = ['\\' + argName, value]
-    return result
+
+global_context = context({
+    'nil': nil,
+    '\\': with_doc(lambda c, argName:
+                       lambda dummy, expr:
+                           create_function(c, argName, expr),
+                   '<special form "\\">'),
+    ':=': with_doc(lambda c, name:
+                       lambda dummy, expr:
+                           global_context.update({
+                               name: evaluate(c, expr)
+                           }) or nil,
+                   '<special form ":=">'),
+})
+
+# REPL functions
+def trampoline (f):
+    while callable(f):
+        f = f()
+    return f
+
 
 def parse (code, start=0):
     tree = ['']
@@ -93,16 +155,6 @@ def parse (code, start=0):
     filtered_tree = filter(lambda x: None if x == '' else x, tree)
     return filtered_tree if start == 0 else (filtered_tree, i)
 
-def interpret(code):
-    syntax_tree = parse(code)
-
-    print syntax_tree
-
-    value = None
-    for expr in syntax_tree:
-        value = evaluate(expr)
-
-    return value
 
 def read_line (prev, open_paren):
     line = raw_input(">> ")
@@ -115,25 +167,23 @@ def read_line (prev, open_paren):
 
     if open_paren == 0:
         try:
-            result = interpret(prev + ' ' + line)
+            result = evaluate(global_context, parse(prev + ' ' + line))
             if result is None:
                 pass
-            elif hasattr(result, 'value'):
-                if isinstance(result.value, str):
-                    print "=>", result.value
-                else:
-                    def reducer(x,y):
-                        if isinstance(y, list):
-                            y = "(" + reduce(reducer, y, '') + ")"
-                        return y if x == '' else x + ' ' + y
-                    print "=>", reduce(reducer, result.value, '')
+            elif isinstance(result, list):
+                def reducer(x,y):
+                    if isinstance(y, list):
+                        y = "(" + reduce(reducer, y, '') + ")"
+                    return y if x == '' else x + ' ' + y
+                print "=>", reduce(reducer, result, '')
             else:
                 print result
-        except Exception, e:
-            print "Error:", e
+        except KeyError, e:
+            print "Value not found:", e
         return lambda: read_line('', 0)
     else:
         return lambda: read_line(prev + ' ' + line, open_paren)
 
+
 if __name__ == '__main__':
-    trampoline(lambda: read_line('',0))
+    trampoline(read_line('',0))
